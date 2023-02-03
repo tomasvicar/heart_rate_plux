8a58f930be5bd4cc3304708bb46b8151eb621b16
tomasvicar <tomasvicar@gmail.com>
2023-02-03 15:34:42 +0100

    sada 02  processed; cleaning

--- untitled3.m
+++ untitled3.m
@@ -1,50 +1,12 @@
 clc;clear all; close all;
 
 
-path = 'D:\data_vo_rate';
+filenames = subdir('../Sada_02/*_dual_m_dual_m*');
 
+for file_num = 1:length(filenames)
+    filename = filenames(file_num).name;
+    filename_out = replace(filename,'_dual_m_dual_m','_dual_m');
 
-rng(42)
-
-data_names = subdir([path '/*.txt']);
-data_names = {data_names.name};
-
-
-heart_rates_oxi1= [];
-heart_rates_oxi2 = [];
-heart_rates_ecg = [];
-
-for file_num = 1:length(data_names)
-
-
-    signal_file_name = data_names{file_num};
-
-
-
-    data = readtable(signal_file_name,'Delimiter',';');
-
-    oxi1 = data.Var6;
-    oxi2 = data.Var7;
-
-    oxi1 = oxi1(20:end);
-    oxi2 = oxi2(20:end);
-
-   
-
-
-    oxi1_filtered = medfilt1(oxi1,40,'truncate');
-    oxi1_filtered = gaussfilt_signal(oxi1_filtered,40);
-%     oxi1_filtered = oxi1_filtered - gaussfilt_signal(oxi1_filtered,150);
-
-    oxi2_filtered = medfilt1(oxi2,40,'truncate');
-    oxi2_filtered = gaussfilt_signal(oxi2_filtered,40);
-%     oxi2_filtered = oxi2_filtered - gaussfilt_signal(oxi2_filtered,150);
-
-    hold off 
-    plot(oxi1_filtered)
-    hold on 
-    plot(oxi2_filtered)
-
-    pause(1)
-
+    movefile(filename, filename_out);
 end
+
